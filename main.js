470047ed3321f41774ba2162367580a8b51a16fb
Martha Mendoza <71796360+Mtc-21@users.noreply.github.com>
2022-01-07 16:00:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector(".changeTheme");
+    const body = document.querySelector("body");
+    const theme = document.querySelector(".themeChange");
+    const cards_overview = document.querySelectorAll(".card-overview")
+    const cards_follow = document.querySelectorAll(".card-follow")
+    let mode = "Dark Mode";
+
+    // change theme
+    btn.addEventListener("input", (e) => {
+      [cards_overview, cards_follow].forEach(cards => {
+        cards.forEach((card, i) => {
+          card.classList.remove("visible")
+        });
+      });
+      body.classList.toggle("light") ? mode = "Light Mode" : mode = "Dark Mode"
+      theme.textContent = mode;
+      setTimeout(showCard, 200);
+
+    });
+    // load cards
+    window.addEventListener('DOMContentLoaded', () => {
+      showCard();
+    });
+
+    function showCard() {
+      [cards_overview, cards_follow].forEach(cards => {
+        cards.forEach((card, i) => {
+          setTimeout(() => {
+            card.classList.add("visible")
+          }, i * 100);
+        });
+      });
+    }
